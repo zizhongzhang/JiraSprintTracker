7df9bc4aea3d9ace0e37cc1ddd2e18736e7f22db
Zizhong <zizhong.zhang43@gmail.com>
2019-12-04 22:05:59 +1100

    WIP

--- myscript.js
+++ myscript.js
@@ -1,21 +1,44 @@
-// Checking page title
-// if (document.title.indexOf("Google") != -1) {
-    //Creating Elements
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://{jira_domain}/rest/agile/1.0/board/{boardId}/sprint?state=active", true);
+if (
+    document.location.href.indexOf(
+        'https://{jira_domain}/secure/RapidBoard.jspa?rapidView=2206'
+    ) != -1
+) {
+    var xhr = new XMLHttpRequest()
+    xhr.open(
+        'GET',
+        'https://{jira_domain}/rest/agile/1.0/board/{boardId}/sprint?state=active',
+        true
+    )
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             // JSON.parse does not evaluate the attacker's scripts.
-            var resp = JSON.parse(xhr.responseText);
-            console.log(resp);
-            localStorage.setItem("test", resp);
+            var resp = JSON.parse(xhr.responseText)
+            console.log(resp)
+            localStorage.setItem('test', resp)
         }
     }
-    xhr.send();
-    
-    var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    //Appending to DOM 
-    document.body.appendChild(btn);
-// }
+    xhr.send()
+
+    xhr.open(
+        'GET',
+        'https://{jira_domain}/rest/agile/1.0/sprint/{sprintId}/issue',
+        true
+    )
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            // JSON.parse does not evaluate the attacker's scripts.
+            let json = JSON.parse(xhr.responseText)
+            let points = json.issues
+                .filter(x => x.fields.status.name == 'Ready for Live')
+                .map(x => x.fields['customfield_10004'])
+                .reduce((a, b) => a + b, 0)
+            console.log(points)
+        }
+    }
+    xhr.send()
+
+    let element = document.querySelector(
+        "div[data-sprint-id='10323'] span.days-left"
+    )
+    element.textContent += ' - 10% (4pts) behind'
+}
