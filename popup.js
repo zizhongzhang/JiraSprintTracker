3e9454bc101703448d4f5bba9d1eef30856a820f
Zizhong Zhang <zizhong.zhang43@gmail.com>
2019-12-05 21:37:56 +1100

    populate stored input

--- popup.js
+++ popup.js
@@ -1,15 +1,23 @@
 'use strict'
-
-const trim = values => values.map((x) => x.trim())
-
+const completeStatusInput = document.getElementById('completeStatus')
+const excludeStatusInput = document.getElementById('excludeStatus')
 const button = document.getElementById('save')
-button.onclick = function() {
-    const completeStatusInput = document.getElementById('completeStatus')
-    const completeStatus = trim(completeStatusInput.value.split(','))
+const body = document.getElementById('body')
 
-    const excludeStatusInput = document.getElementById('excludeStatus')
+body.onload = () => {
+    chrome.storage.sync.get('status', function(data) {
+        const completeStatus = data.status.completeStatus
+        const excludeStatus = data.status.excludeStatus
+        completeStatusInput.value = completeStatus.join() 
+        excludeStatusInput.value = excludeStatus.join()
+    })
+}
+
+const trim = values => values.map(x => x.trim())
+button.onclick = () => {
+    const completeStatus = trim(completeStatusInput.value.split(','))
     const excludeStatus = trim(excludeStatusInput.value.split(','))
-    
+
     chrome.storage.sync.set(
         { status: { completeStatus, excludeStatus } },
         function() {
