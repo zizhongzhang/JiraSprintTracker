911334f9c8816216953d31420852ca2949cab54c
Zizhong Zhang <zizhong.zhang43@gmail.com>
2019-12-05 21:22:15 +1100

    trim input

--- popup.js
+++ popup.js
@@ -1,14 +1,19 @@
 'use strict'
 
+const trim = values => values.map((x) => x.trim())
+
 const button = document.getElementById('save')
 button.onclick = function() {
     const completeStatusInput = document.getElementById('completeStatus')
-    const completeStatus = completeStatusInput.value.split(',')
+    const completeStatus = trim(completeStatusInput.value.split(','))
 
     const excludeStatusInput = document.getElementById('excludeStatus')
-    const excludeStatus = excludeStatusInput.value.split(',')
-
-    chrome.storage.sync.set({ status: { completeStatus, excludeStatus } }, function() {
-        console.log(`Successfully called back`)
-    })
+    const excludeStatus = trim(excludeStatusInput.value.split(','))
+    
+    chrome.storage.sync.set(
+        { status: { completeStatus, excludeStatus } },
+        function() {
+            console.log(`Successfully called back`)
+        }
+    )
 }
