d1992bd4cff130ba5488a70402d5b1f46e479724
zizhong <zizhong.zhang@xero.com>
2019-12-05 14:30:18 +1100

    added UI and options

--- myscript.js
+++ myscript.js
@@ -3,15 +3,19 @@ if (
         'https://{jira_domain}/secure/RapidBoard.jspa?rapidView=2206'
     ) != -1
 ) {
+
+    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+        console.log(response.farewell);
+      });
+      
     var xhr = new XMLHttpRequest()
     xhr.open(
         'GET',
-        'https://{jira_domain}/rest/agile/1.0/board/{boardId}/sprint?state=active',
+        'https://{jira_domain}/rest/agile/1.0/board/2206/sprint?state=active',
         true
     )
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            // JSON.parse does not evaluate the attacker's scripts.
             var resp = JSON.parse(xhr.responseText)
             console.log(resp)
             localStorage.setItem('test', resp)
@@ -21,24 +25,60 @@ if (
 
     xhr.open(
         'GET',
-        'https://{jira_domain}/rest/agile/1.0/sprint/{sprintId}/issue',
+        'https://{jira_domain}/rest/agile/1.0/sprint/10323/issue',
         true
     )
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            // JSON.parse does not evaluate the attacker's scripts.
             let json = JSON.parse(xhr.responseText)
-            let points = json.issues
-                .filter(x => x.fields.status.name == 'Ready for Live')
+            let totalPoints = json.issues
                 .map(x => x.fields['customfield_10004'])
                 .reduce((a, b) => a + b, 0)
-            console.log(points)
+
+            let actualPoints = json.issues
+                .filter(
+                    x =>
+                        x.fields.status.name == 'Ready for Live' ||
+                        x.fields.status.name == 'Done'
+                )
+                .map(x => x.fields['customfield_10004'])
+                .reduce((a, b) => a + b, 0)
+
+            const sprintDuration = 10
+            let element = document.querySelector(
+                "div[data-sprint-id='10323'] span.days-left"
+            )
+            const remainingDays = element.textContent.match(/^\d+/g, '')[0]
+                
+            const expectedPoints = getExpectedPoints(
+                totalPoints,
+                sprintDuration,
+                remainingDays
+            )
+            const pointDiff = actualPoints - expectedPoints
+
+            const expectedPercentage = getPercentage(
+                totalPoints,
+                expectedPoints
+            )
+            const actualPercentage = getPercentage(totalPoints, actualPoints)
+            const percentageDiff = actualPercentage - expectedPercentage
+
+            const message = `- ${Math.abs(percentageDiff)}% (${Math.abs(
+                pointDiff
+            )}pts) ${pointDiff > 0 ? 'ahead' : 'behind'}`
+            
+            element.textContent += message
         }
     }
     xhr.send()
+}
 
-    let element = document.querySelector(
-        "div[data-sprint-id='10323'] span.days-left"
-    )
-    element.textContent += ' - 10% (4pts) behind'
+const getExpectedPoints = (sprintPoints, duration, remainingDays) => {
+    let pointsPerday = sprintPoints / duration
+    return (duration - remainingDays) * pointsPerday
+}
+
+const getPercentage = (sprintPoints, points) => {
+    return (points / sprintPoints) * 100
 }
